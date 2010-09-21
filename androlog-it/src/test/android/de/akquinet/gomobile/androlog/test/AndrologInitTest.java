c504d56ad63bd43de852c43ed848eb012770787b
Clement Escoffier <clement.escoffier@gmail.com>
2010-09-21 15:26:24 +0200

    Initial commit of the IT

---
+++ androlog-it/src/test/android/de/akquinet/gomobile/androlog/test/AndrologInitTest.java
@@ -0,0 +1,299 @@
+/*
+ * Copyright 2010 akquinet
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.akquinet.gomobile.androlog.test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Properties;
+
+import junit.framework.Assert;
+import android.os.Environment;
+import android.test.AndroidTestCase;
+import de.akquinet.android.androlog.Log;
+
+public class AndrologInitTest extends AndroidTestCase {
+
+    private File activate;
+    private File defaultFile;
+    private File deactivate;
+    private File activateAndDefault;
+    private File activateAndLevels;
+    private File subFolder;
+    private File testContext;
+
+    public void setUp() {
+        try {
+            Assert.assertTrue("No SDCard or not the permission to write",
+                    Environment.getExternalStorageDirectory().canWrite());
+            // Create files
+            Properties propsDefault = new Properties();
+            propsDefault.setProperty(Log.ANDROLOG_ACTIVE, "true");
+            defaultFile = new File(Environment.getExternalStorageDirectory(),
+                    Log.ANDROLOG_PROPERTIES);
+            defaultFile.createNewFile();
+            FileOutputStream out = new FileOutputStream(defaultFile);
+            propsDefault.store(out, "Default file");
+            out.close();
+
+            Properties propsActive = new Properties();
+            propsActive.setProperty(Log.ANDROLOG_ACTIVE, "true");
+            activate = new File(Environment.getExternalStorageDirectory(),
+                    "activate.properties");
+            subFolder = new File(Environment.getExternalStorageDirectory(),
+                    "tmp/activate.properties");
+            testContext = new File(Environment.getExternalStorageDirectory(),
+                    getContext().getPackageName() + ".properties");
+            activate.createNewFile();
+            testContext.createNewFile();
+            (new File(Environment.getExternalStorageDirectory(), "tmp"))
+                    .mkdir();
+            subFolder.createNewFile();
+            out = new FileOutputStream(activate);
+            propsActive.store(out, "Enable Androlog file");
+            out.close();
+            out = new FileOutputStream(subFolder);
+            propsActive.store(out, "Enable Androlog file");
+            out.close();
+            out = new FileOutputStream(testContext);
+            propsActive.store(out, "Enable Androlog file");
+            out.close();
+
+            Properties propsDeactive = new Properties();
+            propsDeactive.setProperty(Log.ANDROLOG_ACTIVE, "false");
+            deactivate = new File(Environment.getExternalStorageDirectory(),
+                    "deactivate.properties");
+            deactivate.createNewFile();
+            out = new FileOutputStream(deactivate);
+            propsDeactive.store(out, "Disable Androlog file");
+            out.close();
+
+            Properties propsDefaultLevel = new Properties();
+            propsDefaultLevel.setProperty(Log.ANDROLOG_ACTIVE, "true");
+            propsDefaultLevel.setProperty(Log.ANDROLOG_DEFAULT_LEVEL, "ERROR");
+            activateAndDefault = new File(
+                    Environment.getExternalStorageDirectory(),
+                    "activateAndDefault.properties");
+            activateAndDefault.createNewFile();
+            out = new FileOutputStream(activateAndDefault);
+            propsDefaultLevel.store(out,
+                    "Enable Androlog file and set default log level");
+            out.close();
+
+            Properties propsLevels = new Properties();
+            propsLevels.setProperty(Log.ANDROLOG_ACTIVE, "true");
+            propsLevels.setProperty(Log.ANDROLOG_DEFAULT_LEVEL, "ERROR");
+            propsLevels.setProperty("my.log.verbose", "VERBOSE");
+            propsLevels.setProperty("my.log.debug", "DEBUG");
+            propsLevels.setProperty("my.log.info", "INFO");
+            propsLevels.setProperty("my.log.warn", "WARN");
+            propsLevels.setProperty("my.log.error", "ERROR");
+            propsLevels.setProperty("my.log.assert", "ASSERT");
+            activateAndLevels = new File(
+                    Environment.getExternalStorageDirectory(),
+                    "levels.properties");
+            activateAndLevels.createNewFile();
+            out = new FileOutputStream(activateAndLevels);
+            propsLevels.store(out,
+                    "Enable Androlog file and set default log level");
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    public void tearDown() {
+        // Delete files
+        activate.delete();
+        defaultFile.delete();
+        deactivate.delete();
+        activateAndDefault.delete();
+        activateAndLevels.delete();
+        subFolder.delete();
+        testContext.delete();
+        File sub = new File(Environment.getExternalStorageDirectory(), "tmp");
+        sub.delete();
+    }
+
+    public void testDefaultInit() {
+        Log.init();
+        Assert.assertEquals(Log.INFO, Log.getDefaultLogLevel());
+        String message = "This is a INFO test";
+        String tag = "my.log.info";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.d(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.i(tag, message);
+        Assert.assertEquals(expected, x);
+        x = Log.w(tag, message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testDefaultInitWhenTheFileDoesNotExist() {
+        defaultFile.delete();
+        Log.init();
+        String message = "This is a INFO test";
+        String tag = "my.log.info";
+        int x = Log.d(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.i(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.w(tag, message);
+        Assert.assertEquals(0, x);
+    }
+
+    public void testInitWithActivateFile() {
+        Log.init(activate.getName());
+        String message = "This is a INFO test";
+        String tag = "my.log.info";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.d(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.i(tag, message);
+        Assert.assertEquals(expected, x);
+        x = Log.w(tag, message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testInitWithMissingWile() {
+        Log.init("missing.properties");
+        String message = "This is a INFO test";
+        String tag = "my.log.info";
+        int x = Log.d(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.i(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.w(tag, message);
+        Assert.assertEquals(0, x);
+    }
+
+    public void testInitWithDeactivateFile() {
+        Log.init(deactivate.getName());
+        String message = "This is a INFO test";
+        String tag = "my.log.info";
+        int x = Log.d(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.i(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.w(tag, message);
+        Assert.assertEquals(0, x);
+    }
+
+    public void testInitWithActivateAndDefaultFile() {
+        Log.init(activateAndDefault.getName());
+        Assert.assertEquals(Log.ERROR, Log.getDefaultLogLevel());
+        String message = "This is a INFO test";
+        String tag = "my.log.info";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.d(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.i(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.w(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.e(tag, message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testInitWithFileSettingLevels() {
+        Log.init(activateAndLevels.getName());
+        String message = "This is a VERBOSE test";
+        String tag = "my.log.verbose";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.v(tag, message);
+        Assert.assertEquals(expected, x);
+        x = Log.i(tag, message);
+        Assert.assertEquals(expected, x);
+
+        message = "This is a DEBUG test";
+        tag = "my.log.debug";
+        expected = tag.length() + message.length() + 3;
+        x = Log.v(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.d(tag, message);
+        Assert.assertEquals(expected, x);
+        x = Log.i(tag, message);
+        Assert.assertEquals(expected, x);
+
+        message = "This is a INFO test";
+        tag = "my.log.info";
+        expected = tag.length() + message.length() + 3;
+        x = Log.d(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.i(tag, message);
+        Assert.assertEquals(expected, x);
+        x = Log.w(tag, message);
+        Assert.assertEquals(expected, x);
+
+        message = "This is a WARN test";
+        tag = "my.log.warn";
+        expected = tag.length() + message.length() + 3;
+        x = Log.i(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.w(tag, message);
+        Assert.assertEquals(expected, x);
+        x = Log.e(tag, message);
+        Assert.assertEquals(expected, x);
+
+        message = "This is a ERROR test";
+        tag = "my.log.error";
+        expected = tag.length() + message.length() + 3;
+        x = Log.w(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.e(tag, message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testSubFolderFile() {
+        Log.init("tmp/" + subFolder.getName());
+        String message = "This is a INFO test";
+        String tag = "my.log.info";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.d(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.i(tag, message);
+        Assert.assertEquals(expected, x);
+        x = Log.w(tag, message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testInitWithContext() {
+        Log.init(getContext());
+        String message = "This is a INFO test";
+        String tag = "my.log.info";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.d(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.i(tag, message);
+        Assert.assertEquals(expected, x);
+        x = Log.w(tag, message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testInitWithMissingContext() {
+        testContext.delete();
+        Log.init(getContext());
+        String message = "This is a INFO test";
+        String tag = "my.log.info";
+        int x = Log.d(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.i(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.w(tag, message);
+        Assert.assertEquals(0, x);
+    }
+
+}
---
+++ androlog-it/src/test/android/de/akquinet/gomobile/androlog/test/AndrologTest.java
@@ -0,0 +1,322 @@
+/*
+ * Copyright 2010 akquinet
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.akquinet.gomobile.androlog.test;
+
+import java.util.Properties;
+
+import de.akquinet.android.androlog.Log;
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class AndrologTest extends TestCase {
+
+    public void setUp() {
+        Log.activateLogging();
+        Log.setDefaultLogLevel(Log.VERBOSE);
+    }
+
+    public void testDWithThis() {
+        String tag = "de.akquinet.gomobile.androlog.test.AndrologTest";
+        String message = "This is a DEBUG test";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.d(this, message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testDWithThisWithException() {
+        String tag = "de.akquinet.gomobile.androlog.test.AndrologTest";
+        String message = "This is a DEBUG test";
+        Exception ex = new NullPointerException();
+        int expected = tag.length() + message.length() + 3 + 904; // Size of the
+                                                                    // stack
+                                                                    // trace
+        int x = Log.d(this, message, ex);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testWWithThis() {
+        String tag = "de.akquinet.gomobile.androlog.test.AndrologTest";
+        String message = "This is a WARNING test";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.w(this, message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testWWithThisWithException() {
+        String tag = "de.akquinet.gomobile.androlog.test.AndrologTest";
+        String message = "This is a WARNING test";
+        Exception ex = new NullPointerException();
+        int expected = tag.length() + message.length() + 3 + 904; // Size of the
+                                                                    // stack
+                                                                    // trace
+        int x = Log.w(this, message, ex);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testVWithThis() {
+        String tag = "de.akquinet.gomobile.androlog.test.AndrologTest";
+        String message = "This is a VERBOSE test";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.v(this, message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testVWithThisWithException() {
+        String tag = "de.akquinet.gomobile.androlog.test.AndrologTest";
+        String message = "This is a VERBOSE test";
+        Exception ex = new NullPointerException();
+        int expected = tag.length() + message.length() + 3 + 904; // Size of the
+                                                                    // stack
+                                                                    // trace
+        int x = Log.v(this, message, ex);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testEWithThis() {
+        String tag = "de.akquinet.gomobile.androlog.test.AndrologTest";
+        String message = "This is an ERROR test";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.e(this, message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testEWithThisWithException() {
+        String tag = "de.akquinet.gomobile.androlog.test.AndrologTest";
+        String message = "This is a ERROR test";
+        Exception ex = new NullPointerException();
+        int expected = tag.length() + message.length() + 3 + 904; // Size of the
+                                                                    // stack
+                                                                    // trace
+        int x = Log.e(this, message, ex);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testIWithThis() {
+        String tag = "de.akquinet.gomobile.androlog.test.AndrologTest";
+        String message = "This is a INFO test";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.i(this, message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testIWithThisWithException() {
+        String tag = "de.akquinet.gomobile.androlog.test.AndrologTest";
+        String message = "This is a INFO test";
+        Exception ex = new NullPointerException();
+        int expected = tag.length() + message.length() + 3 + 904; // Size of the
+                                                                    // stack
+                                                                    // trace
+        int x = Log.i(this, message, ex);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testIWithCaller() {
+        String tag = "de.akquinet.gomobile.androlog.test.AndrologTest";
+        String message = "This is a INFO test";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.i(message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testDWithCaller() {
+        String tag = "de.akquinet.gomobile.androlog.test.AndrologTest";
+        String message = "This is a DEBUG test";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.d(message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testWWithCaller() {
+        String tag = "de.akquinet.gomobile.androlog.test.AndrologTest";
+        String message = "This is a WARNING test";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.w(message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testVWithCaller() {
+        String tag = "de.akquinet.gomobile.androlog.test.AndrologTest";
+        String message = "This is a VERBOSE test";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.v(message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testEWithCaller() {
+        String tag = "de.akquinet.gomobile.androlog.test.AndrologTest";
+        String message = "This is an ERROR test";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.e(message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testWhenAdronlogIsDisabled() {
+        Log.deactivateLogging();
+        // V
+        Assert.assertEquals(0, Log.v("xxx"));
+        Assert.assertEquals(0, Log.v(this, "xxx"));
+        Assert.assertEquals(0, Log.v(this, "xxx", new NullPointerException()));
+        Assert.assertEquals(0, Log.v("TAG", "xxx"));
+        Assert.assertEquals(0, Log.v("TAG", "xxx", new NullPointerException()));
+
+        // D
+        Assert.assertEquals(0, Log.d("xxx"));
+        Assert.assertEquals(0, Log.d(this, "xxx"));
+        Assert.assertEquals(0, Log.d(this, "xxx", new NullPointerException()));
+        Assert.assertEquals(0, Log.d("TAG", "xxx"));
+        Assert.assertEquals(0, Log.d("TAG", "xxx", new NullPointerException()));
+
+        // I
+        Assert.assertEquals(0, Log.i("xxx"));
+        Assert.assertEquals(0, Log.i(this, "xxx"));
+        Assert.assertEquals(0, Log.i(this, "xxx", new NullPointerException()));
+        Assert.assertEquals(0, Log.i("TAG", "xxx"));
+        Assert.assertEquals(0, Log.i("TAG", "xxx", new NullPointerException()));
+
+        // W
+        Assert.assertEquals(0, Log.w("xxx"));
+        Assert.assertEquals(0, Log.w(this, "xxx"));
+        Assert.assertEquals(0, Log.w(this, "xxx", new NullPointerException()));
+        Assert.assertEquals(0, Log.w("TAG", "xxx"));
+        Assert.assertEquals(0, Log.w("TAG", "xxx", new NullPointerException()));
+
+        // E
+        Assert.assertEquals(0, Log.e("xxx"));
+        Assert.assertEquals(0, Log.e(this, "xxx"));
+        Assert.assertEquals(0, Log.e(this, "xxx", new NullPointerException()));
+        Assert.assertEquals(0, Log.e("TAG", "xxx"));
+        Assert.assertEquals(0, Log.e("TAG", "xxx", new NullPointerException()));
+    }
+
+    public void testDisablingFromConfiguration() {
+        Log.reset();
+        Properties props = new Properties();
+        props.setProperty(Log.ANDROLOG_ACTIVE, "false");
+        Log.configure(props);
+        // V
+        Assert.assertEquals(0, Log.v("xxx"));
+        Assert.assertEquals(0, Log.v(this, "xxx"));
+        Assert.assertEquals(0, Log.v(this, "xxx", new NullPointerException()));
+        Assert.assertEquals(0, Log.v("TAG", "xxx"));
+        Assert.assertEquals(0, Log.v("TAG", "xxx", new NullPointerException()));
+
+        // D
+        Assert.assertEquals(0, Log.d("xxx"));
+        Assert.assertEquals(0, Log.d(this, "xxx"));
+        Assert.assertEquals(0, Log.d(this, "xxx", new NullPointerException()));
+        Assert.assertEquals(0, Log.d("TAG", "xxx"));
+        Assert.assertEquals(0, Log.d("TAG", "xxx", new NullPointerException()));
+
+        // I
+        Assert.assertEquals(0, Log.i("xxx"));
+        Assert.assertEquals(0, Log.i(this, "xxx"));
+        Assert.assertEquals(0, Log.i(this, "xxx", new NullPointerException()));
+        Assert.assertEquals(0, Log.i("TAG", "xxx"));
+        Assert.assertEquals(0, Log.i("TAG", "xxx", new NullPointerException()));
+
+        // W
+        Assert.assertEquals(0, Log.w("xxx"));
+        Assert.assertEquals(0, Log.w(this, "xxx"));
+        Assert.assertEquals(0, Log.w(this, "xxx", new NullPointerException()));
+        Assert.assertEquals(0, Log.w("TAG", "xxx"));
+        Assert.assertEquals(0, Log.w("TAG", "xxx", new NullPointerException()));
+
+        // E
+        Assert.assertEquals(0, Log.e("xxx"));
+        Assert.assertEquals(0, Log.e(this, "xxx"));
+        Assert.assertEquals(0, Log.e(this, "xxx", new NullPointerException()));
+        Assert.assertEquals(0, Log.e("TAG", "xxx"));
+        Assert.assertEquals(0, Log.e("TAG", "xxx", new NullPointerException()));
+    }
+
+    public void testLogLevelsFromConfiguration() {
+        Log.reset();
+        Properties props = new Properties();
+        props.setProperty(Log.ANDROLOG_ACTIVE, "true");
+        props.setProperty(Log.ANDROLOG_DEFAULT_LEVEL, "ERROR");
+        props.setProperty("my.log.verbose", "VERBOSE");
+        props.setProperty("my.log.debug", "DEBUG");
+        props.setProperty("my.log.info", "INFO");
+        props.setProperty("my.log.warn", "WARN");
+        props.setProperty("my.log.error", "ERROR");
+        props.setProperty("my.log.assert", "ASSERT");
+        Log.configure(props);
+
+        String message = "This is a VERBOSE test";
+        String tag = "my.log.verbose";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.v(tag, message);
+        Assert.assertEquals(expected, x);
+        x = Log.i(tag, message);
+        Assert.assertEquals(expected, x);
+
+        message = "This is a DEBUG test";
+        tag = "my.log.debug";
+        expected = tag.length() + message.length() + 3;
+        x = Log.v(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.d(tag, message);
+        Assert.assertEquals(expected, x);
+        x = Log.i(tag, message);
+        Assert.assertEquals(expected, x);
+
+        message = "This is a INFO test";
+        tag = "my.log.info";
+        expected = tag.length() + message.length() + 3;
+        x = Log.d(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.i(tag, message);
+        Assert.assertEquals(expected, x);
+        x = Log.w(tag, message);
+        Assert.assertEquals(expected, x);
+
+        message = "This is a WARN test";
+        tag = "my.log.warn";
+        expected = tag.length() + message.length() + 3;
+        x = Log.i(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.w(tag, message);
+        Assert.assertEquals(expected, x);
+        x = Log.e(tag, message);
+        Assert.assertEquals(expected, x);
+
+        message = "This is a ERROR test";
+        tag = "my.log.error";
+        expected = tag.length() + message.length() + 3;
+        x = Log.w(tag, message);
+        Assert.assertEquals(0, x);
+        x = Log.e(tag, message);
+        Assert.assertEquals(expected, x);
+    }
+
+    public void testDefaultLevelFromConfiguration() {
+        Log.reset();
+        Properties props = new Properties();
+        props.setProperty(Log.ANDROLOG_ACTIVE, "true");
+        props.setProperty(Log.ANDROLOG_DEFAULT_LEVEL, "ERROR");
+        Log.configure(props);
+
+        String tag = "de.akquinet.gomobile.androlog.test.AndrologTest";
+        String message = "This is an ERROR test";
+        int expected = tag.length() + message.length() + 3;
+        int x = Log.e(this, message);
+        Assert.assertEquals(expected, x);
+
+        expected = tag.length() + message.length() + 3;
+        x = Log.i(this, message);
+        Assert.assertEquals(0, x);
+    }
+
+}
