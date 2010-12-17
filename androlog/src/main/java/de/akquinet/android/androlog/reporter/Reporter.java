91c82c73f3dd863d1ce61557bc0bfbf5d1dd4c5f
Clement Escoffier <clement.escoffier@gmail.com>
2010-12-17 12:03:07 +0100

    Add javadoc Send return a boolean to detect failed sendings

--- androlog/src/main/java/de/akquinet/android/androlog/reporter/Reporter.java
+++ androlog/src/main/java/de/akquinet/android/androlog/reporter/Reporter.java
@@ -2,13 +2,36 @@ package de.akquinet.android.androlog.reporter;
 
 import java.util.Properties;
 
+import de.akquinet.android.androlog.Log;
+
 import android.content.Context;
 
+/**
+ * Reporter Interface.
+ * A Reporter sends a Report to a specific endpoint
+ * (url, mail ...).
+ * Reporter instances are created by {@link Log} according
+ * to the {@link Log#ANDROLOG_REPORT_REPORTERS} configuration
+ * property.
+ */
 public interface Reporter {
 
+    /**
+     * Configures the reporter.
+     * Each reporter can define their own configuration properties.
+     * @param configuration the Androlog configuration.
+     */
     public abstract void configure(Properties configuration);
 
-    public abstract void send(Context context, String message, Throwable error);
+    /**
+     * Asks the reporter to send the report.
+     * @param context the Android context
+     * @param message a message (optional)
+     * @param error a error to attach to the report (optional)
+     * @return <code>true</code> if the report was sent correctly,
+     * <code>false</code> otherwise.
+     */
+    public abstract boolean send(Context context, String message, Throwable error);
 
 
 
